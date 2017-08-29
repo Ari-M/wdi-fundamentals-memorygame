5cfc8d4dcce672fb9b9c95681b6ee45e7e48001d
Ari Minton <ariminton@live.com>
2017-08-29 07:59:30 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 checkForMatch = function () {
+	 this.setAttribute('src', card.cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
@@ -31,16 +32,29 @@ checkForMatch = function () {
 	}
 	
 };
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-}
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+}
+createBoard();
+
+
+
+
 
 
 
